1f7fb8d3a386dfd5a5d260a6bc88feec4e000231
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-01 06:12:07 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -34,7 +34,7 @@ describe('index', () => {
 
   it('calls console.warn()', done => {
     const spy = expect.spyOn(console, 'warn').andCallThrough()
-
+    console.warn("YOU PASSED!")
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
     }, (err, window) => {
