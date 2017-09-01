706ff0247a319c2078dc581890f87f8d3f233a4d
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-01 06:01:57 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -9,7 +9,6 @@ describe('index', () => {
   const src = path.resolve(__dirname, '..', 'index.js')
 
   it('calls console.error()', done => {
-    console.error("HALP!")
     const spy = expect.spyOn(console, 'error').andCallThrough()
 
     jsdom.env(html, [src], {
